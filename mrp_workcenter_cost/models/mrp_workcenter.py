360aaeec2875a10e0779e1c2d2b4c3afe00a3a77
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-01-09 16:29:32 +0200

    Add a new module for calculating work center costs

---
+++ mrp_workcenter_cost/models/mrp_workcenter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class MrpWorkcenter(models.Model):
+
+    _inherit = 'mrp.workcenter'
+
+    service_product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Service product',
+        domain=[('type', '=', 'service')],
+        help='Add a priced service product here to calculate operation costs',
+    )
