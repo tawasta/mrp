c6de797734766b55b77edc0e727ad6a5d318eaef
Timo Talvitie <timo.talvitie@vizucom.com>
2018-01-15 10:18:11 +0200

    Improve field names and positioning

--- mrp_bom_cost/models/mrp_bom.py
+++ mrp_bom_cost/models/mrp_bom.py
@@ -12,22 +12,22 @@ class MrpBom(models.Model):
     def cron_compute_bom_cost(self):
         ''' Triggered by a scheduled action to calculate all BOMs' costs '''
         for bom in self.search([]):
-            bom.bom_cost = self.calculate_cost(bom)
+            bom.component_cost = self.calculate_component_cost(bom)
             bom.cost_updated = datetime.now()
 
     @api.multi
     def action_compute_bom_cost(self):
         ''' Triggered by a button to calculate a single BOM's cost '''
         for bom in self:
-            bom_cost = self.sudo().calculate_cost(bom)
+            component_cost = self.sudo().calculate_component_cost(bom)
             updated = datetime.now()
             self.sudo().write({
-                'bom_cost': bom_cost,
+                'component_cost': component_cost,
                 'cost_updated': updated
             })
 
 
-    def calculate_cost(self, bom):
+    def calculate_component_cost(self, bom):
         ''' Calculate costs for the BOM and its lines '''
         mrp_bom_model = self.env["mrp.bom"]
         cost = 0.0
@@ -35,18 +35,18 @@ class MrpBom(models.Model):
         for line in bom.bom_line_ids:
             if line.child_bom_id:
                 # If line's product has a BOM, call this function recursively until whole line cost is calculated.
-                line_cost = mrp_bom_model.calculate_cost(line.child_bom_id)
+                component_cost = mrp_bom_model.calculate_component_cost(line.child_bom_id)
             else:
                 # If the product does not have a BOM, add the cost price of the product to the BOM costs                
-                line_cost = line.calculate_line_cost()[0]
+                component_cost = line.calculate_component_cost()[0]
 
             # Update cost shown on the line
-            line.line_cost = line_cost
+            line.component_cost = component_cost
 
             # Multiply line cost with product qty and add it to BOM cost
-            cost += line.product_qty * line_cost 
+            cost += line.product_qty * component_cost 
 
         return cost
 
-    bom_cost = fields.Float(digits=dp.get_precision('Product Price'), string="BOM Component Cost")
+    component_cost = fields.Float(digits=dp.get_precision('Product Price'), string="Component Cost")
     cost_updated = fields.Datetime(string="Cost updated", help="Last time BOM cost was updated.")
--- mrp_bom_cost/models/mrp_bom_line.py
+++ mrp_bom_cost/models/mrp_bom_line.py
@@ -8,7 +8,7 @@ class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     @api.one
-    def calculate_line_cost(self):
+    def calculate_component_cost(self):
         ''' Returns the cost of the BOM line. By default fetches the cost from the 
         cost price field of the related product. Override this function for more 
         complex calculations. '''
@@ -16,9 +16,9 @@ class MrpBomLine(models.Model):
         return cost
 
     @api.multi
-    def _get_line_cost_total(self):
+    def _get_component_cost_total(self):
         for line in self:
-            line.line_cost_total = line.line_cost * line.product_qty
+            line.component_cost_total = line.component_cost * line.product_qty
 
-    line_cost = fields.Float(string="Line cost (individual)", digits=dp.get_precision('Product Price'), help='''Cost for 1 BOM line item''')
-    line_cost_total = fields.Float(compute=_get_line_cost_total, string="Line cost (total)", digits=dp.get_precision('Product Price'), help='''Cost for the full quantity of the BOM line's item''')
+    component_cost = fields.Float(string="Component Cost (individual)", digits=dp.get_precision('Product Price'), help='''Component cost for 1 BOM line item''')
+    component_cost_total = fields.Float(compute=_get_component_cost_total, string="Component Cost (total)", digits=dp.get_precision('Product Price'), help='''Component cost for the full quantity of the BOM line's item''')
