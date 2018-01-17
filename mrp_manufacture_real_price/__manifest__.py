b5fd008cf3e4b430f4bb14602f02d769c84fc10a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-01-17 05:53:07 +0200

    Fix category

--- mrp_manufacture_real_price/__manifest__.py
+++ mrp_manufacture_real_price/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Sets real (FIFO) price as manufactured products value',
     'summary': 'Manufactured product value depends on used quants',
     'version': '10.0.1.0.2',
-    'category': 'Events',
+    'category': 'Manufacturing',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
