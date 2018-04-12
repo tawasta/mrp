8a4eaacbb1b1ae249936dcb8656d0fa7ae3d512d
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-12 13:24:40 +0300

    New module mrp_bom_primary_vendor_info

---
+++ mrp_bom_primary_vendor_info/models/mrp_bom_line.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+from odoo.addons import decimal_precision as dp
+
+
+class MrpBomLine(models.Model):
+
+    _inherit = 'mrp.bom.line'
+
+    primary_vendor_id = fields.Many2one(
+        comodel_name='res.partner',
+        related='product_id.primary_supplierinfo_id.name',
+        string="Primary Vendor",
+    )
+
+    primary_vendor_code = fields.Char(
+        related='product_id.primary_supplierinfo_id.product_code',
+        string="Primary Vendor's Code",
+    )
+
+    primary_vendor_price = fields.Float(
+        related='product_id.primary_supplierinfo_id.price',
+        digits=dp.get_precision('Product Price'),
+        string="Primary Vendor's Price",
+    )
+
+    primary_vendor_currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        related='product_id.primary_supplierinfo_id.currency_id',
+        string="Primary Vendor's Currency",
+    )
