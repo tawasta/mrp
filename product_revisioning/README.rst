173790cbb8045ddf6f34a7b2c870e35b21cfc5c1
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-05-03 11:41:55 +0300

    New module product_revisioning

---
+++ product_revisioning/README.rst
@@ -0,0 +1,44 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===============================
+Lightweight Product Revisioning
+===============================
+
+ * Adds a "Create New Revision" button to products that do not have BOMs org
+   variants.
+ * Clicking the button archives the product, creates a duplicate of it, finds
+   all the BOMs the old product was a component of and replaces those products
+   with the new product revision
+ * Old BOMs get archived, get a "Valid Until" date of current date, and get 
+   replaced with a new BOM
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+* The module does not have variant support, and prevents revisioning attempts 
+  if the product has multiple variants.
+
+Credits
+=======
+
+Contributors
+------------
+* Timo Talvitie <timo.talvitie@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ product_revisioning/models/product_template.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _, exceptions
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = 'product.template'
+
+    @api.multi
+    def create_revision(self):
+        self.ensure_one()
+
+        if self.bom_count > 0:
+            msg = _('Revisioning products with BOMs is not allowed')
+            raise exceptions.UserError(msg)
+        if self.product_variant_count > 1:
+            msg = _('Revisioning products with variants is not allowed')
+            raise exceptions.UserError(msg)
+
+        bom_model = self.env['mrp.bom']
+
+        # Create a copy of the current product, mark it as active in case the
+        # old one was not
+        new_product = self.copy()
+        new_product.active = True
+
+        # Find all BOMs that include the current product in the BOM lines
+        matching_boms = bom_model.search(
+            args=[('bom_line_ids.product_id', '=', self.product_variant_id.id)]
+        )
+
+        # Set all BOMs inactive one by one, and create replacement BOMs.
+        # Go through the BOM lines and replace the old product with the new
+        for bom_to_copy in matching_boms:
+            new_bom = bom_to_copy.copy()
+            bom_to_copy.active = False
+
+            for new_bom_line in new_bom.bom_line_ids:
+                if new_bom_line.product_id == self.product_variant_id:
+                    new_bom_line.product_id = new_product.product_variant_id.id
+
+        # Archive the current product and redirect to the new product's
+        # form view
+        self.active = False
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.template',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': new_product.id,
+            'views': [(False, 'form')],
+            'target': 'current',
+        }
