c938fc34583cef4fef742d905f0990ae8743a193
difbit <timo.kekalainen@hotmail.com>
2019-09-20 09:40:14 +0300

    [IMP] Child BOMs

--- material_requirement_planning/models/material_requirement_line.py
+++ material_requirement_planning/models/material_requirement_line.py
@@ -22,6 +22,15 @@ class MaterialRequirementLine(models.Model):
             readonly=False,
             )
 
+    product_uom_id = fields.Many2one(
+            'product.uom',
+            'Product Unit of Measure',
+            )
+
+    promised_qty_line = fields.Char(
+#             string='Promised Quantity',
+            )
+
     variant = fields.Char(
             string='Variant',
             )
@@ -33,6 +42,27 @@ class MaterialRequirementLine(models.Model):
             )
 
     qty_to_manufacture = fields.Float(
+            string="Product Quantity",
+            readonly=False,
+            )
+
+#     manufacturing_level_line = fields.Char(
+# #             _compute='_get_manufacturing_level',
+#             related='material_requirement_id.manufacturing_level.name'
+#             )
+
+
+    manufacturing_level_line = fields.Boolean(
+#             _compute='_get_manufacturing_level',
+            related='material_requirement_id.manufacturing_level'
+            )
+
+#     manufacturing_level_line = fields.Selection(
+# #             _compute='_get_manufacturing_level',
+#             related='material_requirement_id.manufacturing_level'
+#             )
+
+    can_be_manufactured = fields.Float(
             string="Quantity to Manufacture",
             readonly=False,
             )
@@ -45,12 +75,14 @@ class MaterialRequirementLine(models.Model):
 
     bom = fields.Char(string="BoM")
 
-#     bom = fields.Selection(
-#             string="BoM",
-#             )
-
     bom_lines = fields.Many2many(
             comodel_name='mrp.bom.line',
             string='BoM lines',
             readonly=False,
             )
+
+#     @api.multi
+#     def _get_manufacturing_level(self):
+#         print "SELF MANUFACTUGIN LEVEL: ", self.manufacturing_level
+#         self.manufacturing_level = self.material_requirement_id.manufacturing_level
+#
