c53cd373a0166110b79459216bcb3500ce9ca164
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-12-16 17:14:17 +0200

    [ADD] BoM revisioning to product revisioning

--- product_revisioning/models/product_template.py
+++ product_revisioning/models/product_template.py
@@ -1,31 +1,67 @@
 # -*- coding: utf-8 -*-
-from odoo import models, api, _, exceptions
+from odoo import models, api, exceptions, _
+import re
 
 
 class ProductTemplate(models.Model):
 
     _inherit = 'product.template'
 
-    @api.multi
     def create_revision(self):
         self.ensure_one()
 
-        if self.bom_count > 0:
-            msg = _('Revisioning products with BOMs is not allowed')
-            raise exceptions.UserError(msg)
+        default_code = self.get_next_revision_number(self.default_code)
+
+        # Create a copy of the current product, mark it as active in case the
+        # old one was not
+        new_product = self.copy({
+            'name': self.name,
+            'default_code': default_code,
+        })
+        new_product.active = True
+
+        # Archive the current product
+        self.active = False
+
+        view_action = {
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.template',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': new_product.id,
+            'views': [(False, 'form')],
+            'target': 'current',
+        }
+
+        return new_product, view_action
+
+    def get_next_revision_number(self, code):
+        """
+        Get next revision number
+        Only supports suffix ending in numbers only
+        TODO: make generic purpose
+        """
+        suffix_re = '[0-9]+$'
+
+        if re.search(suffix_re, code):
+            sequence = re.search(suffix_re, code).group()
+            new_sequence = str(int(sequence) + 1).zfill(len(sequence))
+
+            return re.sub(suffix_re, new_sequence, code)
+
+        return False
+
+    def action_create_revision(self):
+        self.ensure_one()
+
         if self.product_variant_count > 1:
             msg = _('Revisioning products with variants is not allowed')
             raise exceptions.UserError(msg)
 
-        bom_model = self.env['mrp.bom']
-
-        # Create a copy of the current product, mark it as active in case the
-        # old one was not
-        new_product = self.copy()
-        new_product.active = True
+        new_product, view_action = self.create_revision()
 
         # Find all BOMs that include the current product in the BOM lines
-        matching_boms = bom_model.search(
+        matching_boms = self.env['mrp.bom'].search(
             args=[('bom_line_ids.product_id', '=', self.product_variant_id.id)]
         )
 
@@ -39,15 +75,21 @@ class ProductTemplate(models.Model):
                 if new_bom_line.product_id == self.product_variant_id:
                     new_bom_line.product_id = new_product.product_variant_id.id
 
-        # Archive the current product and redirect to the new product's
-        # form view
-        self.active = False
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'product.template',
-            'view_mode': 'form',
-            'view_type': 'form',
-            'res_id': new_product.id,
-            'views': [(False, 'form')],
-            'target': 'current',
-        }
+        # Redirect to new product view
+        return view_action
+
+    @api.multi
+    def action_create_full_revision(self):
+        view_action = self.action_create_revision()
+
+        for bom in self.bom_ids:
+            reference = self.get_next_revision_number(self.default_code)
+            bom.copy({
+                'product_tmpl_id': view_action.get('res_id'),
+                'code': reference,
+                'product_reference': reference,
+            })
+            bom.active = False
+
+        # Redirect to new product view
+        return view_action
