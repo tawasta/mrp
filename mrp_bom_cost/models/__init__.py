250787d816a5c934994f5330eac7351bc51ce916
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-03-25 09:46:54 +0200

    [REM] Obsolete modules

--- mrp_bom_cost/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import mrp_bom
-from . import mrp_bom_line
-from . import mrp_config_settings
-from . import product_template
-from . import res_company
-
--- mrp_bom_cost_workcenter_support/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import mrp_bom
-from . import mrp_bom_line
-
