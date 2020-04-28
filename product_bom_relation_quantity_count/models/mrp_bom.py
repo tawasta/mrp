1945142c4f23cb53d7aa3eaeba2c3fa98301c5dd
difbit <timo.kekalainen@hotmail.com>
2020-04-28 18:31:09 +0300

    [ADD] Counts product quantites for BOMs

---
+++ product_bom_relation_quantity_count/models/mrp_bom.py
@@ -0,0 +1,24 @@
+
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+
+    _inherit = 'mrp.bom'
+
+    bom_rel_count = fields.Integer(
+        string="Total product count",
+        compute="_compute_bom_rel_count"
+    )
+
+    def _compute_bom_rel_count(self):
+        product_id = self._context.get('active_id')
+        product_model = self._context.get('active_model')
+        if product_model in ['product.product', 'product.template']:
+            product = self.env[product_model].search([('id', '=', product_id)])
+            product = product.product_variant_id or product
+            bom_lines = self.env['mrp.bom.line'].search(
+                [('product_id', '=', product.id)]
+            )
+            for line in bom_lines:
+                line.bom_id.bom_rel_count = line.product_qty
