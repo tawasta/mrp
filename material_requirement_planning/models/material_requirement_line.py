a13f1c9f16a314abf80d74ca81edeeb22339a851
difbit <timo.kekalainen@hotmail.com>
2020-05-11 13:59:18 +0300

    [IMP] Hanging indents fixed

--- material_requirement_planning/models/material_requirement_line.py
+++ material_requirement_planning/models/material_requirement_line.py
@@ -1,6 +1,6 @@
 
 from __future__ import division
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MaterialRequirementLine(models.Model):
@@ -9,70 +9,70 @@ class MaterialRequirementLine(models.Model):
     _description = "Material Requirement Line"
 
     bom = fields.Many2one(
-            comodel_name="mrp.bom",
-            string="BoM",
-            )
+        comodel_name="mrp.bom",
+        string="BoM",
+    )
 
     bom_lines = fields.Many2many(
-            comodel_name="mrp.bom.line",
-            string="BoM lines",
-            readonly=False,
-            )
+        comodel_name="mrp.bom.line",
+        string="BoM lines",
+        readonly=False,
+    )
 
     can_be_manufactured = fields.Float(
-            string="Manufacturable quantity",
-            readonly=False,
-            )
+        string="Manufacturable quantity",
+        readonly=False,
+    )
 
     cost = fields.Float(
-            related="product_id.standard_price"
-            )
+        related="product_id.standard_price"
+    )
 
     incoming = fields.Integer(
-            string="Incoming Qty",
-            compute="_compute_transfer_counts"
-            )
+        string="Incoming Qty",
+        compute="_compute_transfer_counts"
+    )
 
     outgoing = fields.Integer(
-            string="Outgoing Qty",
-            compute="_compute_transfer_counts"
-            )
+        string="Outgoing Qty",
+        compute="_compute_transfer_counts"
+    )
 
     material_requirement_id = fields.Many2one(
-            "material.requirement",
-            string="Material Requirement",
-            readonly=False,
-            )
+        "material.requirement",
+        string="Material Requirement",
+        readonly=False,
+    )
 
     product_id = fields.Many2one(
-            "product.product",
-            string="Product",
-            store=True,
-            readonly=False,
-            )
+        "product.product",
+        string="Product",
+        store=True,
+        readonly=False,
+    )
 
     product_availability = fields.Float(
-            string="On Hand",
-            readonly=False,
-            )
+        string="On Hand",
+        readonly=False,
+    )
 
     product_uom_id = fields.Char(
-            related="product_id.uom_name",
-            string="Product Unit of Measure",
-            )
+        related="product_id.uom_name",
+        string="Product Unit of Measure",
+    )
 
     promised_qty_line = fields.Char(
-            string="Potential quantity",
-            )
+        string="Potential quantity",
+    )
 
     qty_to_manufacture = fields.Float(
-            string="Qty used in BOM",
-            readonly=False,
-            )
+        string="Qty used in BOM",
+        readonly=False,
+    )
 
     variant = fields.Char(
-            string="Variant",
-            )
+        string="Variant",
+    )
 
     def _compute_transfer_counts(self):
         for line in self:
