8e6f1c401fe8a2bad091f128dcec8e08314fefc1
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-07-09 15:02:07 +0300

    [REF] Formatting

--- mrp_workcenter_cost/__manifest__.py
+++ mrp_workcenter_cost/__manifest__.py
@@ -28,8 +28,8 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {"python": [], "bin": [],},
-    "depends": ["mrp",],
-    "data": ["views/mrp_routing_workcenter.xml",],
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["mrp"],
+    "data": ["views/mrp_routing_workcenter.xml"],
     "demo": [],
 }
