1bf24b8d92f2c6137b7d67ac5380f7f39e5c0ce1
difbit <timo.kekalainen@hotmail.com>
2020-11-05 09:10:01 +0200

    [IMP] It is better to use _convert-method

--- mrp_bom_product_cost/models/mrp_bom_line.py
+++ mrp_bom_product_cost/models/mrp_bom_line.py
@@ -6,28 +6,31 @@ class MrpBomLine(models.Model):
 
     _inherit = "mrp.bom.line"
 
-    @api.one
     def calculate_component_cost(self):
-        ''' Returns the cost of the BOM line, using either
+        """ Returns the cost of the BOM line, using either
         -the cost price of the line's product, or
         -the price of the product's primary supplier
         ...converted to the UoM used on the line (with also currency rate
-        conversion if the vendor uses a different currency)'''
+        conversion if the vendor uses a different currency)"""
 
-        if self.bom_id.company_id.bom_cost_calculation_method \
-                == 'vendor_price':
+        if self.bom_id.company_id.bom_cost_calculation_method == 'vendor_price':
             # Use primary vendor's price, fall back to 0 if no vendor exists
-            if not self.product_id.seller_ids:
+            seller_ids = self.product_id.seller_ids.search([
+                ('name.id', '=', self.primary_vendor_id.id),
+                ('product_tmpl_id.id', '=', self.product_id.product_tmpl_id.id)
+            ])
+
+            if not seller_ids:
                 cost = 0.00
             else:
-                primary_vendor = self.product_id.seller_ids[0]
+                primary_vendor = seller_ids[0]
+
+                date = self._context.get('date') or fields.Date.today()
 
                 # Calculate unit cost in primary currency
-                unit_cost_in_eur \
-                    = self.env['res.currency'] \
-                    ._compute(primary_vendor.currency_id,
-                              self.currency_id,
-                              primary_vendor.price)
+                unit_cost_in_eur = self.env['res.currency']._convert(
+                        primary_vendor.price, self.currency_id,
+                        self.bom_id.company_id, date)
 
                 # Calculate cost for the unit used on the BOM.
                 # Note the use of purchase unit of measure instead of
@@ -44,12 +47,12 @@ class MrpBomLine(models.Model):
         return cost
 
     @api.multi
-    def _get_component_cost_total(self):
+    def _compute_component_cost_total(self):
         for line in self:
             line.component_cost_total = line.component_cost * line.product_qty
 
     @api.multi
-    def _get_currency_id(self):
+    def _compute_bom_line_currency_id(self):
         try:
             main_company = self.sudo().env.ref('base.main_company')
         except ValueError:
@@ -63,17 +66,17 @@ class MrpBomLine(models.Model):
     component_cost = fields.Float(
         string="Component Cost per Unit",
         digits=dp.get_precision('Product Price'),
-        help='''Component cost for 1 BOM line item'''
+        help="Component cost for 1 BOM line item"
     )
 
     component_cost_total = fields.Float(
-        compute=_get_component_cost_total,
+        compute="_compute_component_cost_total",
         string="Total Component Cost",
         digits=dp.get_precision('Product Price')
     )
 
     currency_id = fields.Many2one(
         comodel_name='res.currency',
-        compute=_get_currency_id,
-        string='Currency'
+        compute="_compute_bom_line_currency_id",
+        string="Currency"
     )
