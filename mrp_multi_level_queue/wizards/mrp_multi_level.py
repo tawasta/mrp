718348d805b4692f78fa2e5eee76227b8bd5c841
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-06-24 10:43:35 +0300

    [IMP] Run each MRP area separately

--- mrp_multi_level_queue/wizards/mrp_multi_level.py
+++ mrp_multi_level_queue/wizards/mrp_multi_level.py
@@ -12,7 +12,9 @@ class MultiLevelMrp(models.TransientModel):
     def _mrp_cleanup_queued(self, mrp_areas):
         res = super(MultiLevelMrp, self)._mrp_cleanup(mrp_areas)
 
-        job_desc = _("MRP Multi-level: MRP Applicable")
+        job_desc = _(
+            "MRP Multi-level: MRP Applicable for areas {}".format(mrp_areas.ids)
+        )
         self.with_delay(description=job_desc)._calculate_mrp_applicable_queued(
             self.mrp_area_ids
         )
@@ -24,7 +26,9 @@ class MultiLevelMrp(models.TransientModel):
     def _calculate_mrp_applicable_queued(self, mrp_areas):
         res = super(MultiLevelMrp, self)._calculate_mrp_applicable(mrp_areas)
 
-        job_desc = _("MRP Multi-level: MRP Initialisation")
+        job_desc = _(
+            "MRP Multi-level: MRP Initialisation for areas {}".format(mrp_areas.ids)
+        )
         self.with_delay(description=job_desc)._mrp_initialisation_queued(
             self.mrp_area_ids
         )
@@ -36,7 +40,9 @@ class MultiLevelMrp(models.TransientModel):
     def _mrp_initialisation_queued(self, mrp_areas):
         res = super(MultiLevelMrp, self)._mrp_initialisation(mrp_areas)
 
-        job_desc = _("MRP Multi-level: MRP Calculation")
+        job_desc = _(
+            "MRP Multi-level: MRP Calculation for areas {}".format(mrp_areas.ids)
+        )
         self.with_delay(description=job_desc)._mrp_calculation_queued(self.mrp_area_ids)
 
         return res
@@ -47,7 +53,9 @@ class MultiLevelMrp(models.TransientModel):
         mrp_lowest_llc = self._low_level_code_calculation()
         res = super(MultiLevelMrp, self)._mrp_calculation(mrp_lowest_llc, mrp_areas)
 
-        job_desc = _("MRP Multi-level: MRP Final Process")
+        job_desc = _(
+            "MRP Multi-level: MRP Final Process for areas {}".format(mrp_areas.ids)
+        )
         self.with_delay(description=job_desc)._mrp_final_process_queued(
             self.mrp_area_ids
         )
@@ -62,5 +70,6 @@ class MultiLevelMrp(models.TransientModel):
     @api.multi
     @job(default_channel="root.ir_cron")
     def run_mrp_multi_level_queued(self):
-        job_desc = _("MRP Multi-level: MRP Cleanup")
-        self.with_delay(description=job_desc)._mrp_cleanup_queued(self.mrp_area_ids)
+        for area in self.env["mrp.area"].search([]):
+            job_desc = _("MRP Multi-level: MRP Cleanup for '{}'".format(area.name))
+            self.with_delay(description=job_desc)._mrp_cleanup_queued(area)
