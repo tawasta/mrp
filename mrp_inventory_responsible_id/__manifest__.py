4884cccfe2887235f8c2b9ff0e17f003df1748ab
Miika Nissi <miika@miikanissi.com>
2022-07-01 17:26:42 +0300

    [REF] PRE COMMIT

--- mrp_inventory_responsible_id/__manifest__.py
+++ mrp_inventory_responsible_id/__manifest__.py
@@ -23,7 +23,7 @@
     "summary": "Shows product template responsible id in mrp.inventory list view",
     "version": "14.0.1.0.0",
     "category": "Manufacturing",
-    "website": "https://github.com/Tawasta/mrp",
+    "website": "https://gitlab.com/tawasta/odoo/mrp",
     "author": "Tawasta",
     "license": "AGPL-3",
     "application": False,
