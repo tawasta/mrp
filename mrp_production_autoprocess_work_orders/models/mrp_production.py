6944c1f10f1eeb13bfd281b1f6c1f9f1c6436c57
difbit <timo.kekalainen@hotmail.com>
2022-09-16 09:02:26 +0300

    [IMP] Manufacturing order's produced quantities are taken into account

--- mrp_production_autoprocess_work_orders/models/mrp_production.py
+++ mrp_production_autoprocess_work_orders/models/mrp_production.py
@@ -9,11 +9,21 @@ class MrpProduction(models.Model):
         """When work orders are created, immediately go through each WO and
         simulate the click of "Start" and "Done"."""
 
-        # if there are no work orders, create them
+        # If there are no work orders, create them
         if not self.workorder_ids:
             self._create_workorder()
 
         for work_order in self.workorder_ids:
+            # Set WO's Quantity To Be Produced to be
+            # the same as MO's Quantity Producing. This
+            # is needed if less products were produced.
+            work_order.qty_remaining = self.qty_producing
+
             work_order.button_start()
             work_order.button_finish()
+
+        # Set Quantity To Produce to be the same as Quantity Producing.
+        # This means that if less products were produced, then a
+        # manufacturing order is set to Done.
+        self.product_qty = self.qty_producing
         super(MrpProduction, self).button_mark_done()
