2cff19c10dfd413e713fa059df3bcb1cabd0174d
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-10-21 09:14:08 +0000

    [IMP] Update to mrp_bom_cost_cron module. _get_price_from_bom()-function

--- mrp_bom_cost_cron/models/product_product.py
+++ mrp_bom_cost_cron/models/product_product.py
@@ -14,13 +14,21 @@ class ProductProduct(models.Model):
     def _cron_button_bom_cost(self, batch):
         prods = self.env['product.product'].search([('id', 'in', batch)])
         for prod in prods:
-            prod.button_bom_cost()
+            # The function below is used so that it will work with products
+            # with FIFO valuation. button_bom_cost()-function is not used
+            # because with FIFO valuation it will try to open a form view
+            # which would do nothing here.
+            prod.standard_price = prod._get_price_from_bom()
         return batch, 'Success'
 
     @api.multi
     @job
     def cron_button_bom_cost(self):
         """ Computes compute cost for each product """
+
+        # Note that sorting is done in reverse and based on llc-values.
+        # Bigger llc-values means that a product is on a lower level
+        # in the BoM-hierarchy.
         products = self.env['product.product'].search(
             [('bom_ids', '!=', False)]).sorted(
                     key=lambda p: p.llc, reverse=True).ids
