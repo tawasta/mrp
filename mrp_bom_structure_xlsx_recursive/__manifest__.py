952487b4a795ab4143bfcaf17ae548994afb58e5
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2024-05-10 13:22:38 +0000

    [IMP] mrp_bom_structure_xlsx_recursive module: Material notes column moved...

--- mrp_bom_structure_xlsx_recursive/__manifest__.py
+++ mrp_bom_structure_xlsx_recursive/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Life Cycle Analysis report",
     "summary": "Export an excel for LCA report of a BoM",
-    "version": "14.0.1.1.33",
+    "version": "14.0.1.2.33",
     "category": "Manufacturing",
     "website": "https://gitlab.com/tawasta/odoo/mrp",
     "author": "Tawasta",
