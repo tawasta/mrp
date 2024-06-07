d012cde29cf98ac08c5f34d05cb67e10829d9d40
difbit <timo.kekalainen@tawasta.fi>
2024-06-07 16:40:46 +0300

    [IMP] mrp_bom_cost_control_log module: Configure BoM logs on product variants

--- mrp_bom_cost_control_log/models/product_template.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-
-    _inherit = "product.template"
-
-    compute_control_logs = fields.Boolean(string="Compute BoM cost logs")
