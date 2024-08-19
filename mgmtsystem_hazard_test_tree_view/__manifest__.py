f28c167539cacc12ab7187c1234c5f42ae838a1a
difbit <timo.kekalainen@tawasta.fi>
2024-08-19 12:15:27 +0300

    [ADD] The module descriptor file

---
+++ mgmtsystem_hazard_test_tree_view/__manifest__.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2024 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Tree view for Implementation Tests of hazard",
+    "summary": "Creates a tree view for Implementation Tests of hazard",
+    "version": "14.0.1.0.0",
+    "category": "Health",
+    "website": "https://gitlab.com/tawasta/odoo/mrp",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mgmtsystem",
+        "mgmtsystem_hazard",
+    ],
+    "data": [
+        "views/mgmtsystem_hazard_test.xml",
+    ],
+}
