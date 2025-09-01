32704c583cf287ee97c372f7655a5fc283fd7582
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-01 14:25:53 +0300

    If mrp.move has no mrp_order_number use origin

--- mrp_area_mrp_move_customer_name/models/mrp_move.py
+++ mrp_area_mrp_move_customer_name/models/mrp_move.py
@@ -13,5 +13,8 @@ class MrpMove(models.Model):
             if record.mrp_order_number:
                 sale_order = record.env['sale.order'].search([('name', '=', record.mrp_order_number)])
                 record.customer_name = sale_order.partner_id.name
+            if record.origin:
+                sale_order = record.env['sale.order'].search([('name', '=', record.origin)])
+                record.customer_name = sale_order.partner_id.name
             else:
                 record.customer_name = ""
