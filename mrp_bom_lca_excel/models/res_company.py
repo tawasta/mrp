ca0306b5e5158a099cc839a12fa4724ba3439b80
difbit <timo.kekalainen@tawasta.fi>
2025-09-29 08:14:00 +0300

    [IMP] mrp_bom_lca_excel module: Settings to hide certain sheets from LCA bom excel

--- mrp_bom_lca_excel/models/res_company.py
+++ mrp_bom_lca_excel/models/res_company.py
@@ -5,3 +5,15 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     time_in_year = fields.Integer(string="Seconds in a year")
+    hide_by_product_sheet = fields.Boolean(
+        string="Hide by-products Sheet",
+    )
+    hide_operation_sheet = fields.Boolean(
+        string="Hide operations, energy, consumption Sheet",
+    )
+    hide_requirement_sheet = fields.Boolean(
+        string="Hide product requirements Sheet",
+    )
+    hide_summary_sheet = fields.Boolean(
+        string="Hide Material summaries Sheet",
+    )
--- mrp_bom_lca_excel/models/res_config_settings.py
+++ mrp_bom_lca_excel/models/res_config_settings.py
@@ -10,3 +10,27 @@ class ResConfigSettings(models.TransientModel):
         readonly=False,
         help="Set the time parameter for LCA bom excel",
     )
+    hide_by_product_sheet = fields.Boolean(
+        string="Hide by-products Sheet",
+        related="company_id.hide_by_product_sheet",
+        readonly=False,
+        help="Activate to hide by-products Sheet in LCA bom excel",
+    )
+    hide_operation_sheet = fields.Boolean(
+        string="Hide operations, energy, consumption Sheet",
+        related="company_id.hide_operation_sheet",
+        readonly=False,
+        help="Activate to hide operations Sheet in LCA bom excel",
+    )
+    hide_requirement_sheet = fields.Boolean(
+        string="Hide product requirements Sheet",
+        related="company_id.hide_requirement_sheet",
+        readonly=False,
+        help="Activate to hide requirements Sheet in LCA bom excel",
+    )
+    hide_summary_sheet = fields.Boolean(
+        string="Hide Material summaries Sheet",
+        related="company_id.hide_summary_sheet",
+        readonly=False,
+        help="Activate to hide summary Sheet in LCA bom excel",
+    )
