ea0bdf8db9038c2a8b6cdd10dc5598eeb5f4639e
difbit <timo.kekalainen@tawasta.fi>
2026-02-19 11:42:04 +0200

    [FIX] mrp_create_purchase_from_work_order module: Fixed the error by removing api.model from one function

--- mrp_create_purchase_from_work_order/__manifest__.py
+++ mrp_create_purchase_from_work_order/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Create Purchases from Work Orders",
     "summary": "Create Purchases from Work Orders",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "MRP",
     "website": "https://github.com/tawasta/mrp",
     "author": "Futural",
